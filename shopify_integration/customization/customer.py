b7e51ccb98f8b8b103db7dcaa5d098ce4d134e9d
anasoutpost <anas.outpost@gmail.com>
2026-07-27 11:56:53 +0530

    Update customer.py

--- shopify_integration/customization/customer.py
+++ shopify_integration/customization/customer.py
@@ -1,41 +1,45 @@
 import frappe
 from frappe.custom.doctype.custom_field.custom_field import create_custom_field
 
+
 def create_custom_fields():
     custom_fields = {
         "Customer": [
-             {
-    {
-        "fieldname": "shopify_shop",
-        "fieldtype": "Data",
-        "label": "Shopify Shop",
-        "insert_after": "loyalty_program_tier",
-        "read_only": 0,
-        "no_copy": 1,
-    },
-    {
-        "fieldname": "shopify_customer_id",
-        "fieldtype": "Data",
-        "label": "Shopify Customer ID",
-        "insert_after": "shopify_shop",
-        "read_only": 0,
-        "unique": 1,
-        "no_copy": 1,
-    },
-    {
-        "fieldname": "shopify_email",
-        "fieldtype": "Data",
-        "label": "Shopify Email",
-        "insert_after": "shopify_customer_id",
-        "read_only": 0,
-        "no_copy": 1,
-    },
+            {
+                "fieldname": "shopify_shop",
+                "fieldtype": "Data",
+                "label": "Shopify Shop",
+                "insert_after": "loyalty_program_tier",
+                "read_only": 0,
+                "no_copy": 1,
+            },
+            {
+                "fieldname": "shopify_customer_id",
+                "fieldtype": "Data",
+                "label": "Shopify Customer ID",
+                "insert_after": "shopify_shop",
+                "read_only": 0,
+                "unique": 1,
+                "no_copy": 1,
+            },
+            {
+                "fieldname": "shopify_email",
+                "fieldtype": "Data",
+                "label": "Shopify Email",
+                "insert_after": "shopify_customer_id",
+                "read_only": 0,
+                "no_copy": 1,
+            },
         ]
     }
 
     for doctype, fields in custom_fields.items():
         for field in fields:
-            existing = frappe.db.exists("Custom Field", {"dt": doctype, "fieldname": field["fieldname"]})
+            existing = frappe.db.exists(
+                "Custom Field",
+                {"dt": doctype, "fieldname": field["fieldname"]}
+            )
+
             if not existing:
                 create_custom_field(doctype, field)
             else:
@@ -44,16 +48,31 @@ def create_custom_fields():
                     if key not in ("fieldname", "fieldtype"):
                         setattr(cf, key, value)
                 cf.save(ignore_permissions=True)
-            frappe.db.commit()
-            frappe.clear_cache(doctype=doctype)
+
+        frappe.db.commit()
+        frappe.clear_cache(doctype=doctype)
 
 
 def delete_custom_fields():
-    custom_fields_to_delete = { "Customer": ["shopify_shop", "shopify_customer_id","shopify_email"] }
+    custom_fields_to_delete = {
+        "Customer": [
+            "shopify_shop",
+            "shopify_customer_id",
+            "shopify_email",
+        ]
+    }
 
-    for doctype, fields in custom_fields_to_delete.items(): 
-        for field_name in fields: 
-            if frappe.db.exists("Custom Field", {"dt": doctype, "fieldname": field_name}): 
-                frappe.delete_doc("Custom Field", f"{doctype}-{field_name}", ignore_missing=True) 
-                frappe.db.commit() 
-                frappe.clear_cache(doctype=doctype)      
+    for doctype, fields in custom_fields_to_delete.items():
+        for field_name in fields:
+            if frappe.db.exists(
+                "Custom Field",
+                {"dt": doctype, "fieldname": field_name}
+            ):
+                frappe.delete_doc(
+                    "Custom Field",
+                    f"{doctype}-{field_name}",
+                    ignore_missing=True,
+                )
+
+        frappe.db.commit()
+        frappe.clear_cache(doctype=doctype)
