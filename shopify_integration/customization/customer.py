dc5421e6727af2b113be12016a90acaa9e858446
anasoutpost <anas.outpost@gmail.com>
2026-07-27 11:47:21 +0530

    Update customer.py

--- shopify_integration/customization/customer.py
+++ shopify_integration/customization/customer.py
@@ -5,17 +5,11 @@ def create_custom_fields():
     custom_fields = {
         "Customer": [
              {
-        "fieldname": "shopify_section",
-        "fieldtype": "Section Break",
-        "label": "Shopify",
-        "insert_after": "loyalty_program_tier",
-        "collapsible": 1,
-    },
     {
         "fieldname": "shopify_shop",
         "fieldtype": "Data",
         "label": "Shopify Shop",
-        "insert_after": "shopify_section",
+        "insert_after": "loyalty_program_tier",
         "read_only": 1,
         "no_copy": 1,
     },
@@ -55,7 +49,7 @@ def create_custom_fields():
 
 
 def delete_custom_fields():
-    custom_fields_to_delete = { "Customer": ["shopify_section", "shopify_shop", "shopify_customer_id","shopify_email"] }
+    custom_fields_to_delete = { "Customer": ["shopify_shop", "shopify_customer_id","shopify_email"] }
 
     for doctype, fields in custom_fields_to_delete.items(): 
         for field_name in fields: 
